0533d8cd394aeb0fa0318096dc7d79b823e43b85
non258 <c0117037bf@edu.teu.ac.jp>
2018-05-06 16:37:56 +0900

    ファイルをきりわけ

--- bin/colter.js
+++ bin/colter.js
@@ -1,26 +1,5 @@
 #!/usr/bin/env node
-//console.log('Hello, World!');
 
-let argv = require('argv');
-let package = require('../package.json');
+let opt = require("./options");
 
-argv.option([{
-  name: 'none',
-  short:'n',
-  type: 'string',
-  description:'テスト表示',
-  example: "'colter --none' or 'colter -n'"
-},
-
-{
-  name: 'vvvv',
-  short:'v',
-  type: 'string',
-  description:'バージョンの表示',
-  example: "'colter --version' or 'colter -v'"
-}]);
-
-argv.version(package.version);
-
-console.log(argv.run());
-
+console.log(opt.run());
