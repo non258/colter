ca12d3eba694dda5ef071deaa21d52d420235b4f
non258 <c0117037bf@edu.teu.ac.jp>
2018-05-06 16:06:35 +0900

    helpの追加とその使い方練習

--- bin/colter.js
+++ bin/colter.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-console.log('Hello, World!');
+//console.log('Hello, World!');
+
+let argv = require('argv');
+let package = require('../package.json');
+
+argv.option([{
+  name: 'none',
+  short:'n',
+  type: 'string',
+  description:'テスト表示',
+  example: "'colter --none' or 'colter -n'"
+},
+
+{
+  name: 'vvvv',
+  short:'v',
+  type: 'string',
+  description:'バージョンの表示',
+  example: "'colter --version' or 'colter -v'"
+}]);
+
+argv.version(package.version);
+
+console.log(argv.run());
+
