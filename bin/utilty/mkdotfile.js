57a0311163ce75fdf38c9280c729c6e777791d61
non258 <c0117037bf@edu.teu.ac.jp>
2018-06-14 13:15:26 +0900

    mkdotfile.js

---
+++ bin/utilty/mkdotfile.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const os = require('os')
+let filename = ".colter"
+
+function mkdot()
+{
+  try
+  {
+    let dir = fs.readdirSync(`${os.homedir()}/${filename}`)
+
+  }
+  catch(e)
+  {
+    fs.mkdirSync(`${os.homedir()}/${filename}`)
+  }
+}
