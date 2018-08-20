d91ccb0c7435b8a669abb3a88675fb770b0e7ced
non258 <c0117037bf@edu.teu.ac.jp>
2018-08-20 19:28:58 +0900

    JSONを読み込む

--- bin/utilty/read_json.js
+++ bin/utilty/read_json.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const os = require('os')
 const settings = require('../settings.js')
 
-fs.readFile(`${os.homedir()}/${settings.rc}/coloersetting.json`, "utf-8", function (err, text) {
-  console.log(text)
+fs.readFile(`${os.homedir()}/${settings.rc}/${settings.colorrc}`, "utf-8", function (err, text) {
   let setting_json = JSON.parse(text)
   console.log(setting_json)
 })
