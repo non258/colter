75cf36e0114dca659b82575d76ca97d5148943df
ctare <sankaku900622@yahoo.co.jp>
2018-08-20 16:49:00 +0900

    rm 'none' and 'vvvv'

--- bin/options.js
+++ bin/options.js
@@ -6,22 +6,6 @@ argv.info(`Usage: colter [pattern] [color]`)
 
 argv.option([
 {
-  name: 'none',
-  short:'n',
-  type: 'string',
-  description:'テスト表示',
-  example: "'colter --none' or 'colter -n'"
-},
-
-{
-  name: 'vvvv',
-  short:'v',
-  type: 'string',
-  description:'バージョンの表示',
-  example: "'colter --version' or 'colter -v'"
-},
-
-{
   name: 'init',
   type: 'bool',
   description: '初期処理を行います',
