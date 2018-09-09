48de61e432f8ab20bead0451c90bd5e0cacb38b3
non258 <c0117037bf@edu.teu.ac.jp>
2018-09-09 21:17:04 +0900

    関数への切り分けとexports

--- bin/utility/read_json.js
+++ bin/utility/read_json.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
+const fs = require('fs')
 const os = require('os')
 const settings = require('../settings.js')
 
-fs.readFile(`${os.homedir()}/${settings.rc}/${settings.colorrc}`, "utf-8", function (err, text) {
-  let setting_json = JSON.parse(text)
+function read_settings_json () {
+  let setting_json = JSON.parse(fs.readFileSync(`${os.homedir()}/${settings.rc}/${settings.colorrc}`, "utf-8"))
   console.log(setting_json)
-})
+  return setting_json
+}
 
+module.exports = read_settings_json
