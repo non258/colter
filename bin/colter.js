fd76bab3380bd462e65b15876dd172279c570489
non258 <c0117037bf@edu.teu.ac.jp>
2018-09-09 21:19:04 +0900

    read_settings_jsonとconvert_dircolorsをrequire

--- bin/colter.js
+++ bin/colter.js
@@ -2,8 +2,12 @@
 
 const opt = require("./options");
 const ch = require("chalk");
-const chalk_convert = require("./utilty/convert");
-const mkdot = require("./utilty/mkdotfile")
+const chalk_convert = require("./utility/convert")
+const mkdot = require("./utility/mkdotfile")
+const read_settings_json = require("./utility/read_json") 
+const convert_dircolors = require("./utility/convert_dircolors") 
+
+
 let data = opt.run();
 let pattern = data.targets[0];
 let color = data.targets[1];
