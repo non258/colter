cb9e53d982ffaa7babc2fa4ab517af8bca1cd10b
non258 <c0117037bf@edu.teu.ac.jp>
2018-09-11 21:15:58 +0900

    dircolorsの設定を取得

---
+++ bin/utility/get_dircolors_settings.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const os = require('os')
+const settings = require('../settings')
+
+
+function get_dircolors_settings() {
+  console.log('get')
+  try
+  {
+    let file = fs.readFileSync(`${os.homedir()}/${settings.rc}/${settings.colorrc}`)
+    console.log('file', file)
+  }
+  catch(e)
+  {
+    const execSync = require('child_process').execSync
+    const set =  execSync('dircolors -p').toString()
+    fs.writeFile(`${os.homedir()}/${settings.rc}/${settings.colorrc}`, set, (err) => {})
+  }
+}
+
+module.exports = get_dircolors_settings
