20943fef81be8b4da4a148dc4f0a64bc3135f6ba
non258 <c0117037bf@edu.teu.ac.jp>
2018-09-11 21:25:06 +0900

    return の追加

--- bin/utility/get_dircolors_settings.js
+++ bin/utility/get_dircolors_settings.js
@@ -4,17 +4,17 @@ const settings = require('../settings')
 
 
 function get_dircolors_settings() {
-  console.log('get')
   try
   {
     let file = fs.readFileSync(`${os.homedir()}/${settings.rc}/${settings.colorrc}`)
-    console.log('file', file)
+    return file.toString()
   }
   catch(e)
   {
     const execSync = require('child_process').execSync
     const set =  execSync('dircolors -p').toString()
     fs.writeFile(`${os.homedir()}/${settings.rc}/${settings.colorrc}`, set, (err) => {})
+    return set
   }
 }
 
