1cb1963bf1dc35b0e80de1a85001d2d83f67039e
ctare <sankaku900622@yahoo.co.jp>
2018-09-29 01:35:50 +0900

    色をテンプレートから選べるようにする

--- bin/utility/create_color.js
+++ bin/utility/create_color.js
@@ -9,8 +9,19 @@ function create_color (color) {
     return chalk.rgb(rgb[1], rgb[2], rgb[3])
   }
 
-  let hex = color.match(/#?([0-9a-f]+)/)
-  if (color[0] != '#') {
+  let template = {
+    red: [255, 0, 0],
+    green: [0, 255, 0],
+    blue: [0, 0, 255],
+    yellow: [255, 255, 0],
+  }
+  let template_color = template[color]
+  if (template_color != undefined) {
+    return chalk.rgb(template_color[0], template_color[1], template_color[2])
+  }
+
+  let hex = color.match(/^#?([0-9a-f]+)$/)
+  if (hex) {
     return chalk.hex("#" + hex[1])
   }
 
