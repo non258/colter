f768c0f2dacd6f0287bc457e185fa63146c56305
non258 <c0117037bf@edu.teu.ac.jp>
2019-05-05 21:45:39 +0900

    ラムダ式に修正

--- bin/config_browser/build_server.js
+++ bin/config_browser/build_server.js
@@ -6,31 +6,33 @@ const querystring = require('querystring')
 const read_config_req = require("./read_config_req")
 
 function build_server() {
-  server.on('request', response)
+  server.on('request', 
+    (req, res)  => {
+      res.writeHead(200, {'Content-Type' : 'text/html'})
+      if (req.url == '/') {
+        fs.readFile(`${require('path').dirname(require.main.filename)}/config_browser/index.html`, 'utf-8', 
+          (err, data) => {
+            res.write(data)
+            res.end()
+          })
 
-  function response(req, res) {
-    res.writeHead(200, {'Content-Type' : 'text/html'})
-    if (req.url == '/') {
-      fs.readFile(`${require('path').dirname(require.main.filename)}/config_browser/index.html`, 'utf-8', filedata)
-      function filedata(err, data){
-        res.write(data)
-        res.end()
+      } else if (req.url == '/post') {
+        // res.write('post color')
+        req.on('data', data => {
+          color_data = querystring.parse(data.toString())
+          console.log(color_data.req_coloer);
+
+          // TODO: 受け取った値をcolterに渡し設定ファイルを書きかえる
+          //     : 別途関数を作成する -> read_confg_req
+          read_config_req(color_data.req_coloer)
+
+          res.writeHead(301, {Location: '/'})
+          res.end()
+        })
       }
-    } else if (req.url == '/post') {
-      // res.write('post color')
-      req.on('data', data => {
-        color_data = querystring.parse(data.toString())
-        console.log(color_data.req_coloer);
-
-        // TODO: 受け取った値をcolterに渡し設定ファイルを書きかえる
-        //     : 別途関数を作成する -> read_confg_req
-        read_config_req(color_data.req_coloer)
-
-        res.writeHead(301, {Location: '/'})
-        res.end()
-      })
     }
-  }
+  )
+
 
   server.listen(8080, 'localhost')
 }
